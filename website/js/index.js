711cd5b37acae7fcf9e1d6b274c8056f46e56712
Vivek Shah <vivekshah9969@gmail.com>
2020-11-08 19:00:54 +0530

    adding new website

---
+++ website/js/index.js
@@ -0,0 +1,60 @@
+const base_url = "http://127.0.0.1:5000"
+
+
+function showData(data){
+    console.log("got final data")
+    console.log(data)
+    document.getElementById("thisisresult").innerText = JSON.stringify(data);
+}
+
+$(document).ready(function(){
+
+    console.log("Loaded")
+    data_count = 0
+    finaldata = {}
+
+    $("#dataInputForm").on('submit',function(e){
+        e.preventDefault()
+        const github_handle = $("#github_handle").val()
+        const linkedin_handle = $("#linkedin_handle").val()
+
+        let linkedin_dataurl = `${base_url}/linkedin/${linkedin_handle}`
+        let github_dataurl = `${base_url}/github/${github_handle}`
+
+        console.log(linkedin_dataurl)
+        console.log(github_dataurl)
+
+        $.ajax({
+            url: linkedin_dataurl, 
+            timeout	: 40*1000,
+            success: function(result){
+                console.log("got linkedin data")
+                console.log(result)
+                data_count += 1
+                finaldata["linkedin"] = result
+                if(data_count == 2){
+                    showData(finaldata)
+                    data_count = 0
+                }
+            }
+        });
+
+        $.ajax({
+            url: github_dataurl, 
+            timeout	: 40*1000,
+            success: function(result){
+                console.log("got github data")
+                console.log(result)
+                data_count += 1
+                finaldata["github"] = result
+                if(data_count == 2){
+                    showData(finaldata)
+                    data_count = 0
+                }
+            }
+        });
+
+        return false
+    });
+})
+
