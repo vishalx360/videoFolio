1a30ad1515ed4a6137d75e7cbea7a5ab4fb34749
Vivek Shah <vivekshah9969@gmail.com>
2020-11-08 14:42:10 +0530

    added linkedin to data_service

---
+++ data_service/app.py
@@ -0,0 +1,21 @@
+from linkedin import *
+from github import *
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route('/linkedin/<handle>')
+def get_linkedin(handle):
+    print("Started for " + handle)
+    profile =  get_profile(handle)
+    print("got profile")
+    if profile is None:
+        abort(404, description="Profile not found")
+    # li_data = profile
+    li_data = process_profile(profile)
+    print("processed done")
+    return li_data
+
+@app.route('/')
+def health():
+    return "healthy"
