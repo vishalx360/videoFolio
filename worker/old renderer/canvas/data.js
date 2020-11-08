916a51e84def331e32d94d4cb8fa808c12419b72
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2020-11-08 21:40:05 +0530

    add generator

---
+++ worker/old renderer/canvas/data.js
@@ -0,0 +1,54 @@
+{	
+	“name” : “Vivek SHah”,
+	“headline”: “Computer engineering student at DDU”,
+	“github_username”: “vivekshah1801”,
+	“linkedin_profile”: “https://linkedin.com/in/vivekshah1801”,
+	“profile_url”: “<link to image>”,
+	“email” : “vivekshah9969@gmail.com”,
+	“blog_link” : “” [Optional],
+	“website” : “https://vivekshah.tech” [Optional],
+	“twitter_link” : “twitter.com/vivekshah” [Optional],
+“education” : [
+		{
+			“institute”: “Dharmsinh Desai University”,
+			“course” : “B.Tech - Computer Engineering”,
+			“start_date” : “01 may 2017”,
+			“end_date” : “01 may 2021” | 0,
+		},
+		....
+],
+	“work_exp” : [
+		{
+			“company”: “Dharmsinh Desai University”,
+			“title” : “B.Tech - Computer Engineering”,
+			“start_date” : “20 may 2020”,
+			“end_date” : “20 september 2020” | 0,
+			“description” : “This is an OPTIONAL description”,
+		},
+		....
+],
+repo_count : 10,
+git_company_name : “FOSS ASIA”,
+total_stars : 129,
+popular_repos : [
+	{
+	name: “Batman”,
+	stars: 1245,
+	},
+	....
+]
+	total_contributions : 37,
+	languages: [
+	“python”,
+	“C”,
+	“Javascript”,
+	....
+],
+pull_request_count: 7,
+popular_contributions : [
+	{
+		repo_link : “https://github.com/otheruser/otherrepo”,
+		stars : 1300,
+	}
+],
+}
